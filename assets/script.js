0b7f396b362138623b547f5a8f819a93cd43a9e4
Nicole Barkley <barkleylikecharles@icloud.com>
2022-04-08 01:11:39 -0400

    current weather append to HTML

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,9 @@
-// Add variables
-// var weatherFormEl = document.querySelector("#weather-form")
-// var cityNameEl = document.querySelector("#searchInput")
-// var currentContainer = document.querySelector("#currentWeather");
-
-// var formHandler = function(event) {
-//     event.preventDefault();
-
-//     var searchInput = cityNameEl.value.trim();
-//     if (searchInput) {
-    // getCurrentWeather(cityName)
-    
-// }
 
 // Create variables for handling & storing the search form inputs 
 var searchFormEl = document.querySelector("#search-form");
 var cityInputEl = document.querySelector("#cityName");
+var currentWeatherContainerEl = document.querySelector("#current-weather-container");
+var citySearchTerm = document.querySelector("#city-search-form");
 
 // Add submit functionality
 var formSubmitHandler = function(event) {
@@ -28,8 +17,14 @@ var formSubmitHandler = function(event) {
         } else {
          alert("Please enter a City Name in the search bar");
         }
-    console.log(event);
+        // Save city search input to call back later from list
+    //     saveCity();
+    // console.log(event);
   };
+//Function to save the city searches to local storage
+// var saveCity = function(){
+//     localStorage.setItem("cities", JSON.stringify(cities));
+// };
 // Variable to store API key to utilize in two different API calls
 var weatherApiKey = "86796ec06d4bd2ee10aac5628e1bb374"
 // Function to fetch the API data for current weather forecast using city name search functionality
@@ -39,8 +34,48 @@ var getCurrentWeather = function (cityName) {
     fetch(urlCurrent).then(function(response) {
         console.log(response);
         response.json().then(function(data) {
-            console.log(data);
+            displayCurrentWeather(data, cityName);
+            // console.log(data);
         });
     });
 };
 searchFormEl.addEventListener("submit", formSubmitHandler);
+
+// Function to display the current weather 
+var displayCurrentWeather = function(weather, citySearch) {
+    console.log(weather);
+    console.log(citySearch);
+    // Clear old input from search bar
+    currentWeatherContainerEl.textContent = "";
+    citySearchTerm.textContent = citySearch;
+
+   //Use moment to check and return current date for display
+   var currentDate = document.createElement("span")
+   currentDate.textContent=" (" + moment(weather.dt.value).format("MMMM D, YYYY") + ") ";
+   citySearchTerm.appendChild(currentDate);
+
+  //Utilize open weather api's weather icons and append 
+   var weatherIcon = document.createElement("img")
+   weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`);
+   currentWeatherContainerEl.appendChild(weatherIcon);
+
+   //Temperature display utilizing a span element
+   var tempEl = document.createElement("span");
+   tempEl.textContent = "Current Temp: " + weather.main.temp + " °F";
+   tempEl.classList = "list-group-item"
+   currentWeatherContainerEl.appendChild(tempEl);
+
+   //Humidity display utilizing a span element
+   var humidityEl = document.createElement("span");
+   humidityEl.textContent = "Humidity: " + weather.main.humidity + " %";
+   humidityEl.classList = "list-group-item";
+   currentWeatherContainerEl.appendChild(humidityEl);
+
+   //Wind Speed display utilizing a span element
+   var windSpeedEl = document.createElement("span");
+   windSpeedEl.textContent = "Wind Speed: " + weather.wind.speed + " MPH";
+   windSpeedEl.classList = "list-group-item";
+   currentWeatherContainerEl.appendChild(windSpeedEl);
+  
+  };
+
