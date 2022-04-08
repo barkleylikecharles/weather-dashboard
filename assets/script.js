33cc0eb13620d2fc02ff8ee0c56091624729dd16
Nicole Barkley <barkleylikecharles@icloud.com>
2022-04-07 22:28:31 -0400

    working current weather api

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,27 @@
 // Add variables
-var weatherFormEl = document.querySelector("#weather-form")
-var cityNameEl = document.querySelector("#searchInput")
-var currentContainer = document.querySelector("#repos-container");
-var extendedContainer = document.querySelector()
+// var weatherFormEl = document.querySelector("#weather-form")
+// var cityNameEl = document.querySelector("#searchInput")
+// var currentContainer = document.querySelector("#currentWeather");
+
+// var formHandler = function(event) {
+//     event.preventDefault();
+
+//     var searchInput = cityNameEl.value.trim();
+//     if (searchInput) {
+    // getCurrentWeather(cityName)
+    
+// }
+
+var weatherApiKey = "86796ec06d4bd2ee10aac5628e1bb374"
+// Get longitute and latitude 
+var getCurrentWeather = function (cityName) {
+    var urlCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName+ "&appid=" + weatherApiKey + "&units=imperial";
+
+    fetch(urlCurrent).then(function(response) {
+        console.log(response);
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+};
+getCurrentWeather("hartford");
